7b2bcd2b8755fdc9bf96a5b56d84bb7b3a61857f
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-06-14 17:32:07 +0300

    added codegen loader and locale keys

---
+++ lib/localizations/locale_keys.g.dart
@@ -0,0 +1,8 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const home_home_title = 'home.home_title';
+  static const home_hello_txt = 'home.hello_txt';
+  static const home = 'home';
+
+}
