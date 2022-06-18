4bbc6b65fa8e87039e57b2d56fa9b36472252b03
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-06-18 21:54:05 +0300

    screen util init

--- lib/app.dart
+++ lib/app.dart
@@ -4,17 +4,21 @@ import 'package:flutter_basic_template/constants/app_strings.dart';
 import 'package:flutter_basic_template/screens/home_screen.dart';
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appTitle,
-      theme: AppTheme().appTheme,
-      locale: context.locale,
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      home: HomeScreen(),
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      builder: (BuildContext context, Widget? child) => MaterialApp(
+        title: appTitle,
+        theme: AppTheme().appTheme,
+        locale: context.locale,
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        home: HomeScreen(),
+      ),
     );
   }
 }
