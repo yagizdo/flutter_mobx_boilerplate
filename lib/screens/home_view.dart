462051a836c771288b2ff608868b117d96c6a3c2
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-07 21:55:37 +0300

    added list on homeview

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_basic_template/constants/app_colors.dart';
 import 'package:flutter_basic_template/constants/app_strings.dart';
 import 'package:flutter_basic_template/localizations/locale_keys.g.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -13,26 +14,17 @@ class HomeView extends StatelessWidget {
       appBar: AppBar(
         title: Text(LocaleKeys.home_home_title.tr()),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            // Home Text
-            Text(LocaleKeys.home_hello_txt.tr(),style: Theme.of(context).textTheme.headline1,),
-
-            // Sizedbox for padding
-            SizedBox(height: 20.h),
-
-            // Change Language Button
-            ElevatedButton(onPressed: (){
-              // if lang == en change to tr else change to en
-              context.locale == Locale('en') ? context.setLocale(Locale('tr')) : context.setLocale(Locale('en'));
-
-              // Print current language
-              print(context.locale.toString());
-            }, child: Text(LocaleKeys.home_home_btn.tr()),),
-          ],
-        )
-      ),
+      body: Column(
+        children: [
+          Text('List', style: TextStyle(color: white, fontSize: ScreenUtil().setSp(20))),
+          Expanded(child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) => ListTile(
+              title: Text('Item $index',style: TextStyle(color: white),),
+            ),
+          )),
+        ],
+      )
     );
   }
 }
