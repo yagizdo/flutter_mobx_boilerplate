f11c0940196ddd97b961d774e3bf19e71d36fa80
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-07-07 22:03:03 +0300

    added dio and network client

---
+++ lib/core/network_client.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+
+class NetworkClient {
+
+  // BaseURL of the API
+  String baseUrl = 'https://jsonplaceholder.typicode.com';
+
+  // Dio instance
+  Dio dio = Dio();
+}
